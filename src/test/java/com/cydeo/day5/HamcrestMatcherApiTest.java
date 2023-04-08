facd6adb654c8c7225de6f2a07e1cd2a9504e193
Ekrem Meşe <ekremmese1@gmail.com>
2023-04-08 07:13:02 +0300

    initial commit

---
+++ src/test/java/com/cydeo/day5/HamcrestMatcherApiTest.java
@@ -0,0 +1,96 @@
+package com.cydeo.day5;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class HamcrestMatcherApiTest {
+
+    @DisplayName("OnePartan with Hamcrest and chaining")
+    @Test
+    public void test1() {
+
+
+        /*
+     Given accept type is json
+     And path param id is 10
+     When user sends a get request to "api/spartans/{id}"
+     Then status code is 200
+     And content-type is "application/json"
+     And response payload values match the following:
+         {
+    "id": 15,
+    "name": "Meta",
+    "gender": "Female",
+    "phone": 1938695106
+            }
+   */
+
+        //this is called chaining
+
+        given().accept(ContentType.JSON)
+                .and()
+                    .pathParam("id", 15)
+                .when()
+                    .get("http://54.87.214.234:8000/api/spartans/{id}")
+                .then()
+                .statusCode(200)
+                .and()
+                .contentType("application/json")
+                .and()
+                .body("id",equalTo(15),
+                        "name",is("Meta"),
+                        "gender", is("Female"),
+                        "phone", is(1938695106));
+
+
+    }
+
+    @DisplayName("GET request to teacher/all and chaining")
+    @Test
+    public void teachersTest(){
+
+        given()
+                    .accept(ContentType.JSON)
+                .and()
+                    .pathParam("id", 5)
+                .when()
+                    .get("https://api.training.cydeo.com/teacher/{id}")
+                .then()
+                    .statusCode(200)
+                .and()
+                    .contentType("application/json;charset=UTF-8")
+                .and()
+                    .header("Date", notNullValue())
+                .and()
+                    .body("teachers[0].firstName", is("Mario"),
+                        "teachers[0].lastName", is("Luigi"),
+                        "teachers[0].gender", is("Male"));
+
+
+
+    }
+
+    @DisplayName("GET request to teacher/aşş and chaining")
+    @Test
+    public void test3(){
+
+        //verify "Valter","Mario","Porter")
+
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .get("https://api.training.cydeo.com/teacher/all")
+                .then()
+                .statusCode(200)
+                .and()
+                .body("teachers.firstName", hasItems("Valter","Mario","Porter"));
+
+    }
+
+
+}
--- src/test/java/com/cydeo/day5/HamcrestMatchersIntro.java
+++ src/test/java/com/cydeo/day5/HamcrestMatchersIntro.java
@@ -1,6 +1,15 @@
 package com.cydeo.day5;
 
+import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 
 public class HamcrestMatchersIntro {
 
@@ -9,6 +18,84 @@ public class HamcrestMatchersIntro {
 
         //Hamcrest is an assertion library. Asserting with plain english.
 
+        //actual 5+5
+        assertThat(5 + 5, is(2 + 8));   // --> plain english
+
+        assertThat(5 + 5, equalTo(10));
+
+        //this is
+        assertThat(4 + 4, is(equalTo(8)));
+
+        assertThat(4 + 6, not(11));
+        assertThat(5 + 5, is(not(11)));
+        assertThat(5 + 5, is(not(equalTo(11))));
+
+        //number comparison
+
+        //greaterThan()
+        //greaterThanOrEqualTo()
+        //lessThan()
+        //lessThanOrEqualTo()
+
+
+    }
+
+    @DisplayName("Assertion with String")
+    @Test
+    public void stringHamcrest() {
+
+        String text = "B22 is learning Hamcrest";
+
+        //checking for equality is same as numbers
+        assertThat(text, is("B22 is learning Hamcrest"));
+        assertThat(text, equalTo("B22 is learning Hamcrest"));
+        assertThat(text, is(equalTo("B22 is learning Hamcrest")));
+
+        //check if this text starts with B22
+        assertThat(text, startsWith("B22"));
+
+        //case insensitive
+        assertThat(text, startsWithIgnoringCase("b22"));
+
+        //ends with
+        assertThat(text, endsWith("rest"));
+
+        //check if text contains String learning
+        assertThat(text, containsString("learning"));
+
+        //contains insensitive
+        assertThat(text, containsStringIgnoringCase("LEARNING"));
+
+        String str = " ";
+        assertThat(str,blankString());
+        assertThat(str,is(blankString()));
+
+        //check if trimmed str is empty string
+        assertThat(str.trim(), emptyString());
+
+
+
+    }
+
+    @DisplayName("Hamcrest for Collection")
+    @Test
+    public void testCollection(){
+
+        List<Integer> listOfNumbers= Arrays.asList(1,4,5,6,32,54,66,77,45,23);
+
+        //check size of the list
+        assertThat(listOfNumbers, hasSize(10));  // ---> we don't need to even say listOfNumber.size();
+
+        //check if this list hasItem 77
+        assertThat(listOfNumbers,hasItem(77));
+
+        //check if this list has items 77,54 and 23
+        assertThat(listOfNumbers, hasItems(77,54,23));
+
+        assertThat(listOfNumbers, is(hasItem(23)));  // ---> that also works with is(hasItem())
+
+        //check if all numbers greater than
+        assertThat(listOfNumbers, everyItem(greaterThan(5)));  //--> magical method, no need loops for
 
 
     }
