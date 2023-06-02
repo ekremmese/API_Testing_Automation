bd7138ee0f24348ec7d2a25a8ba1db0caaa8917e
Ekrem Meşe <ekremmese1@gmail.com>
2023-06-02 20:04:52 +0300

    Day7 Post Request Started!

---
+++ src/test/java/com/cydeo/utilities/SpartanUtils.java
@@ -0,0 +1,14 @@
+package com.cydeo.utilities;
+
+public class SpartanUtils {
+
+
+
+}
+
+
+//Create one SpartanUtil class
+//create a static method that returns Map<String,Object>
+//use faker library(add as a depedency) to assign each time different information
+//for name,gender,phone number
+//then use your method for creating spartan as a map,dynamically.
